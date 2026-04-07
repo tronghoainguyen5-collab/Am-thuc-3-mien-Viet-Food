5ee9811db1392b398d881372ca8df7437db5617b
tronghoainguyen5-collab <tronghoainguyen5@gmail.com>
2026-04-07 16:57:05 +0700

    abc

--- backend/src/server.js
+++ backend/src/server.js
@@ -32,7 +32,9 @@ require("dotenv").config();
 const app = express();
 
 // middleware
-app.use(cors());
+app.use(cors({
+  origin: "http://127.0.0.1:5500"
+}));
 app.use(express.json());
 
 // routes
